607f5d538e9b0d416f6048e7ecc219645f0184dd
HabaruWasTaken <64592358+HabaruWasTaken@users.noreply.github.com>
2023-06-14 09:40:54 +0000

    some style

--- routes/pages.js
+++ routes/pages.js
@@ -4,37 +4,38 @@ const router = express.Router();
 
 const logout = require('../controllers/logout');
 
-router.get('/', loggedIn, (req, res)=>{
-    if(req.user){
-      res.render('pages/index', { status: "loggedIn", user: req.user });
-      
-    } else {
-        res.render('pages/index', { status: "no", user: "nothing" });
-    }
+router.get('/', loggedIn, (req, res) => {
+  if (req.user) {
+    res.render('pages/index', { status: "loggedIn", user: req.user });
+
+  } else {
+    res.render('pages/index', { status: "no", user: "nothing" });
+  }
 });
 
-router.get('/register', (req, res)=>{
-    res.sendFile('register.html', {root: "./public/"});
+router.get('/register', (req, res) => {
+  res.render('pages/register', { status: 'no', user: "nothing" });
 });
 
-router.get('/login', (req, res)=>{
-    res.sendFile('login.html', {root: "./public/"});
+router.get('/login', (req, res) => {
+  // res.sendFile('login.html', {root: "./public/"});
+  res.render('pages/login', { status: 'no', user: "nothing" })
 });
 
-router.get('/profile', loggedIn, (req, res)=>{
-    if(req.user){
-        res.render('pages/profile', { status: "loggedIn", user: req.user });
-    } else {
-        res.redirect('/');
-    }
+router.get('/profile', loggedIn, (req, res) => {
+  if (req.user) {
+    res.render('pages/profile', { status: "loggedIn", user: req.user });
+  } else {
+    res.redirect('/');
+  }
 });
 
-router.get('/profile/edit', loggedIn, (req, res)=>{
-    if(req.user){
-        res.render('pages/editProfile', { status: "loggedIn", user: req.user });
-    } else {
-        res.redirect('/');
-    }
+router.get('/profile/edit', loggedIn, (req, res) => {
+  if (req.user) {
+    res.render('pages/editProfile', { status: "loggedIn", user: req.user });
+  } else {
+    res.redirect('/');
+  }
 });
 
 router.get('/logout', logout);
